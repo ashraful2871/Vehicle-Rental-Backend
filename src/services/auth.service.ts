071dd9ddbf2332831ffb389fdf0b4e6b73fc9061
Md. Ashraful Islam <ashrafulislam2758@gmail.com>
2026-08-17 22:45:12 +0600

    make the auth services file

---
+++ src/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { StaffRepository } from "../repositories/staff.repository";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "../db/knexfile";
+export class AuthService {
+  private staffRepository: StaffRepository;
+
+  constructor() {
+    this.staffRepository = new StaffRepository();
+  }
+  async login(email: string, password: string): Promise<string | null> {
+    const staff = await this.staffRepository.findByEmail(email);
+    if (!staff) {
+      return null;
+    }
+
+    const isMatch = await bcrypt.compare(password, staff.password_hash);
+    if (!isMatch) {
+      return null;
+    }
+    const token = jwt.sign(
+      { id: staff.id, email: staff.email },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "2d" },
+    );
+
+    return token;
+  }
+}
